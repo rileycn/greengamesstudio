80a96824e06c84c42944ae5cab379c744c835a9a
rileycn <rileycnoggle@gmail.com>
2025-03-15 14:51:56 -0400

    base game nearly done

---
+++ Assets/Scripts/WindEffectManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+public class WindEffectManager : MonoBehaviour
+{
+    private Color ogcolor = Color.gray;
+    public float startTime;
+
+    public TMP_Text clickText;
+
+    public PlantManager pm;
+
+    private float clickTime = 3f;
+
+    public List<SpriteRenderer> usedSprites = new();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int timeLeft = Mathf.CeilToInt(clickTime - (Time.time - startTime));
+        if (timeLeft <= -1) {
+            pm.Die();
+            gameObject.SetActive(false);
+        } else {
+            clickText.text = "Click! (" + timeLeft + ")";
+        }
+    }
+
+    public void Activate()
+    {
+        foreach (SpriteRenderer sr in usedSprites)
+        {
+            sr.color = ogcolor;
+        }
+        startTime = Time.time;
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnMouseEnter()
+    {
+        foreach (SpriteRenderer sr in usedSprites)
+        {
+            sr.color = new Color(ogcolor.r * 0.5f, ogcolor.g * 0.5f, ogcolor.b * 0.5f, ogcolor.a);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        foreach (SpriteRenderer sr in usedSprites)
+        {
+            sr.color = ogcolor;
+        }
+    }
+}
